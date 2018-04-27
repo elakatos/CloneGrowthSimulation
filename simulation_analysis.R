74a56f45d945d591ef77e01d8b4689c74c71d48f
elakatos <e.lakatos@qmul.ac.uk>
2018-04-27 13:04:40 +0100

    Comparative plots of simulation results

--- simulation_analysis.R
+++ simulation_analysis.R
@@ -1,7 +1,16 @@
 
-dirList = c('18_04_24_2', '18_04_05_1')
+dirList = c('18_04_26_3', '18_04_26_4', '18_04_25_3', '18_04_25_4')
 
+p <- ggplot()
+p2 <- ggplot() +scale_x_continuous(limits=c(10, 200)) + scale_y_continuous(limits=c(0.04, 1))
 
+rsqDF <- data.frame(matrix(vector(), nrow=200))
+mutrDF <- data.frame(matrix(vector(), nrow=200))
+
+colList = c('skyblue3', 'darksalmon', 'burlywood3', 'darkseagreen3', 'mediumorchid3')
+i=1
+
+for (dir in dirList){
 
 vafdata <- read.table(paste0('~/CRCdata/Simulation_results/',dir,'/Vafdf.csv'), sep=',', header=T, row.names=1)
 
@@ -12,14 +21,28 @@ vafSummary <- data.frame(invf = as.numeric(row.names(vafdata)), avg = apply(vafd
 vafdata$invf <- as.numeric(row.names(vafdata))
 vafInd <- melt(vafdata, id='invf')
 
-p <- ggplot() +
-  geom_ribbon(data=vafSummary, aes(x = invf,ymin = avg - 2*sdev, ymax = avg+ 2*sdev),fill='skyblue3', alpha=0.3) +
-  geom_line(data=vafInd, aes(x=invf, y=value, group=variable),colour='grey20',alpha=0.1) +
+
+rsqDF[, dir] <- rsq
+mutrDF[, dir] <- mutrs
+if (startsWith(dir, '18_04_25')){mutrDF[,dir] <- mutrDF[,dir]/2}
+
+p <- p +
+  geom_ribbon(data=vafSummary, aes(x = invf,ymin = avg - 2*sdev, ymax = avg+ 2*sdev),fill=colList[i], alpha=0.3) +
+  #geom_line(data=vafInd, aes(x=invf, y=value, group=variable),colour='grey20',alpha=0.1) +
   theme_bw() + geom_line(data=vafSummary, aes(x=invf, y=avg),size=1.2)
 
-p2 <- ggplot(vafSummary, aes(x=invf, y=sdev/avg)) + geom_line() + theme_bw() +
-  scale_x_continuous(limits=c(30, 200)) + scale_y_continuous(limits=c(0.04, 0.12))
+p2 <- p2 +
+  geom_line(data=vafSummary, aes(x=invf, y=sdev/avg), colour=colList[i]) + theme_bw()
 
-p3 <- ggplot() + aes(x='', y=rsq) + geom_violin(fill='slategray3') + coord_flip() + theme_bw()
+i = i+1
 
-p4 <- ggplot() + geom_violin(aes(x='', y=mutrs),fill='firebrick3') + theme_bw()
+}
+
+mutrM <- melt(mutrDF)
+rsqM <- melt(rsqDF)
+
+p3 <- ggplot(rsqM, aes(x=variable, y=value, fill=variable)) + geom_violin() +
+  coord_flip() + theme_bw() + scale_fill_manual(values=colList)
+
+p4 <- ggplot(mutrM, aes(x=variable, y=value, fill=variable)) +
+  geom_violin() + theme_bw() + scale_fill_manual(values=colList)
